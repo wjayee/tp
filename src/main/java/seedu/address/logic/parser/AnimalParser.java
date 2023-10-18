4603ab80b3a2e8ddb625d59903104ad0ec0b717a
Blizzeracz <lamkwangzheloysius@gmail.com>
2023-10-18 13:13:08 +0800

    Create AnimalParser interface

---
+++ src/main/java/seedu/address/logic/parser/AnimalParser.java
@@ -0,0 +1,16 @@
+package seedu.address.logic.parser;
+
+import seedu.address.logic.commands.AnimalCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Represents a Parser that is able to parse user input into a {@code Command} of type {@code T}.
+ */
+public interface AnimalParser<T extends AnimalCommand> {
+
+    /**
+     * Parses {@code userInput} into a command and returns it.
+     * @throws ParseException if {@code userInput} does not conform the expected format
+     */
+    T parse(String userInput) throws ParseException;
+}
