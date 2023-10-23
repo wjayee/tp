9f0b0fff58c083b3be40bcfdfa650d46274dc09f
Blizzeracz <lamkwangzheloysius@gmail.com>
2023-10-24 00:27:39 +0800

    Fix issues

--- src/main/java/seedu/address/logic/AnimalMessages.java
+++ src/main/java/seedu/address/logic/AnimalMessages.java
@@ -13,11 +13,19 @@ import seedu.address.model.animal.Animal;
 public class AnimalMessages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
-    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n$s";
     public static final String MESSAGE_INVALID_ANIMAL_DISPLAYED_INDEX = "The animal index provided is invalid";
-    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d animals listed!";
+    public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "$d animals listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
             "Multiple values specified for the following single-valued field(s): ";
+    public static final String PET_ID = "ID";
+    public static final String SEX = "Sex";
+    public static final String SPECIES = "Species";
+    public static final String BREED = "Breed";
+    public static final String DATE_OF_BIRTH = "Date of Birth";
+    public static final String ADMISSION_DATE = "Date of Admission";
+    public static final String DISPLAY_FORMAT = "; %s:";
+
 
     /**
      * Returns an error message indicating the duplicate prefixes.
@@ -35,19 +43,19 @@ public class AnimalMessages {
      * Formats the {@code animal} for display to the user.
      */
     public static String format(Animal animal) {
-        final StringBuilder builder = new StringBuilder();
+        StringBuilder builder = new StringBuilder();
         builder.append(animal.getName())
-                .append("; ID: ")
+                .append(String.format(DISPLAY_FORMAT, PET_ID))
                 .append(animal.getPetId())
-                .append("; Sex: ")
+                .append(String.format(DISPLAY_FORMAT, SEX))
                 .append(animal.getSex())
-                .append("; Species: ")
+                .append(String.format(DISPLAY_FORMAT, SPECIES))
                 .append(animal.getSpecies())
-                .append("; Breed: ")
+                .append(String.format(DISPLAY_FORMAT, BREED))
                 .append(animal.getBreed())
-                .append("; DOB: ")
+                .append(String.format(DISPLAY_FORMAT, DATE_OF_BIRTH))
                 .append(animal.getDateOfBirth())
-                .append("; DOA: ")
+                .append(String.format(DISPLAY_FORMAT, ADMISSION_DATE))
                 .append(animal.getAdmissionDate());
         return builder.toString();
     }
