5f4b7fa466d78504c7c3a941978149de35414752
Blizzeracz <lamkwangzheloysius@gmail.com>
2023-10-24 15:43:15 +0800

    Add valid and invalid attributes in the AnimalCommandTest utility class

--- src/test/java/seedu/address/logic/commands/AnimalCommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/AnimalCommandTestUtil.java
@@ -2,11 +2,13 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+import static seedu.address.logic.parser.CliAnimalSyntax.PREFIX_BREED;
+import static seedu.address.logic.parser.CliAnimalSyntax.PREFIX_DATE_OF_ADMISSION;
+import static seedu.address.logic.parser.CliAnimalSyntax.PREFIX_DATE_OF_BIRTH;
+import static seedu.address.logic.parser.CliAnimalSyntax.PREFIX_ID;
+import static seedu.address.logic.parser.CliAnimalSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliAnimalSyntax.PREFIX_SEX;
+import static seedu.address.logic.parser.CliAnimalSyntax.PREFIX_SPECIES;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import java.util.ArrayList;
@@ -19,55 +21,68 @@ import seedu.address.model.AnimalCatalog;
 import seedu.address.model.AnimalModel;
 import seedu.address.model.animal.Animal;
 import seedu.address.model.animal.NameContainsKeywordsPredicate;
-import seedu.address.testutil.EditPersonDescriptorBuilder;
 
 /**
  * Contains helper methods for testing commands.
  */
 public class AnimalCommandTestUtil {
 
-    public static final String VALID_NAME_AMY = "Amy Bee";
-    public static final String VALID_NAME_BOB = "Bob Choo";
-    public static final String VALID_PHONE_AMY = "11111111";
-    public static final String VALID_PHONE_BOB = "22222222";
-    public static final String VALID_EMAIL_AMY = "amy@example.com";
-    public static final String VALID_EMAIL_BOB = "bob@example.com";
-    public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
-    public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
-    public static final String VALID_TAG_HUSBAND = "husband";
-    public static final String VALID_TAG_FRIEND = "friend";
+    public static final String VALID_NAME_POOKIE = "Pookie";
+    public static final String VALID_NAME_TOFU = "Tofu";
+    public static final String VALID_ID_POOKIE = "1111";
+    public static final String VALID_ID_TOFU = "1234";
+    public static final String VALID_DATE_OF_BIRTH_POOKIE = "2020-12-12";
+    public static final String VALID_DATE_OF_BIRTH_TOFU = "2019-10-10";
+    public static final String VALID_ADMISSION_DATE_POOKIE = "2023-12-12";
+    public static final String VALID_ADMISSION_DATE_TOFU = "2019-11-11";
+    public static final String VALID_SEX_POOKIE = "MALE";
+    public static final String VALID_SEX_TOFU = "FEMALE";
+    public static final String VALID_SPECIES_POOKIE = "Dog";
+    public static final String VALID_SPECIES_TOFU = "Cat";
+    public static final String VALID_BREED_POOKIE = "Poodle";
+    public static final String VALID_BREED_TOFU = "British Shorthair";
 
-    public static final String NAME_DESC_AMY = " " + PREFIX_NAME + VALID_NAME_AMY;
-    public static final String NAME_DESC_BOB = " " + PREFIX_NAME + VALID_NAME_BOB;
-    public static final String PHONE_DESC_AMY = " " + PREFIX_PHONE + VALID_PHONE_AMY;
-    public static final String PHONE_DESC_BOB = " " + PREFIX_PHONE + VALID_PHONE_BOB;
-    public static final String EMAIL_DESC_AMY = " " + PREFIX_EMAIL + VALID_EMAIL_AMY;
-    public static final String EMAIL_DESC_BOB = " " + PREFIX_EMAIL + VALID_EMAIL_BOB;
-    public static final String ADDRESS_DESC_AMY = " " + PREFIX_ADDRESS + VALID_ADDRESS_AMY;
-    public static final String ADDRESS_DESC_BOB = " " + PREFIX_ADDRESS + VALID_ADDRESS_BOB;
-    public static final String TAG_DESC_FRIEND = " " + PREFIX_TAG + VALID_TAG_FRIEND;
-    public static final String TAG_DESC_HUSBAND = " " + PREFIX_TAG + VALID_TAG_HUSBAND;
 
-    public static final String INVALID_NAME_DESC = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
-    public static final String INVALID_PHONE_DESC = " " + PREFIX_PHONE + "911a"; // 'a' not allowed in phones
-    public static final String INVALID_EMAIL_DESC = " " + PREFIX_EMAIL + "bob!yahoo"; // missing '@' symbol
-    public static final String INVALID_ADDRESS_DESC = " " + PREFIX_ADDRESS; // empty string not allowed for addresses
-    public static final String INVALID_TAG_DESC = " " + PREFIX_TAG + "hubby*"; // '*' not allowed in tags
 
+    public static final String INDEX_NAME_POOKIE = " " + PREFIX_NAME + VALID_NAME_POOKIE;
+    public static final String INDEX_NAME_TOFU = " " + PREFIX_NAME + VALID_NAME_TOFU;
+    public static final String INDEX_ID_POOKIE = " " + PREFIX_ID + VALID_ID_POOKIE;
+    public static final String INDEX_ID_TOFU = " " + PREFIX_ID + VALID_ID_TOFU;
+    public static final String INDEX_DATE_OF_BIRTH_POOKIE = " " + PREFIX_DATE_OF_BIRTH + VALID_DATE_OF_BIRTH_POOKIE;
+    public static final String INDEX_DATE_OF_BIRTH_TOFU = " " + PREFIX_DATE_OF_BIRTH + VALID_DATE_OF_BIRTH_TOFU;
+    public static final String INDEX_ADMISSION_DATE_POOKIE = " " + PREFIX_DATE_OF_ADMISSION
+            + VALID_ADMISSION_DATE_POOKIE;
+    public static final String INDEX_ADMISSION_DATE_TOFU = " " + PREFIX_DATE_OF_ADMISSION + VALID_ADMISSION_DATE_TOFU;
+    public static final String INDEX_SEX_POOKIE = " " + PREFIX_SEX + VALID_SEX_POOKIE;
+    public static final String INDEX_SEX_TOFU = " " + PREFIX_SEX + VALID_SEX_TOFU;
+    public static final String INDEX_SPECIES_POOKIE = " " + PREFIX_SPECIES + VALID_SPECIES_POOKIE;
+    public static final String INDEX_SPECIES_TOFU = " " + PREFIX_SPECIES + VALID_SPECIES_TOFU;
+    public static final String INDEX_BREED_POOKIE = " " + PREFIX_BREED + VALID_BREED_POOKIE;
+    public static final String INDEX_BREED_TOFU = " " + PREFIX_BREED + VALID_BREED_TOFU;
+
+    public static final String INDEX_INVALID_NAME = " " + PREFIX_NAME + "James&"; // '&' not allowed in names
+    public static final String INDEX_INVALID_ID = " " + PREFIX_ID + "911a"; // 'a' not allowed in ID
+    public static final String INDEX_INVALID_DATE_OF_BIRTH = " " + PREFIX_DATE_OF_BIRTH + "2022-13-10"; // no such date
+    public static final String INDEX_INVALID_DATE_OF_ADMISSION = " "
+            + PREFIX_DATE_OF_ADMISSION + "22-13-2022"; // wrong date format
+    public static final String INDEX_INVALID_SEX = " " + PREFIX_SEX + "others"; // Can only be male or female
+    public static final String INDEX_INVALID_SPECIES = " " + PREFIX_SPECIES
+            + "Waterboard2"; // '2' not allowed in species
+    public static final String INDEX_INVALID_BREED = " " + PREFIX_BREED + "Water-board"; // '-' not allowed in breed
     public static final String PREAMBLE_WHITESPACE = "\t  \r  \n";
     public static final String PREAMBLE_NON_EMPTY = "NonEmptyPreamble";
 
-    public static final EditCommand.EditPersonDescriptor DESC_AMY;
-    public static final EditCommand.EditPersonDescriptor DESC_BOB;
-
-    static {
-        DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
-                .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-                .withTags(VALID_TAG_FRIEND).build();
-        DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
-                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
-                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
-    }
+    //    public static final EditCommand.EditPersonDescriptor DESC_AMY;
+    //    public static final EditCommand.EditPersonDescriptor DESC_BOB;
+    //
+    //    static {
+    //        DESC_AMY = new EditPersonDescriptorBuilder().withName(VALID_NAME_AMY)
+    //                .withPhone(VALID_PHONE_AMY).withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
+    //                .withTags(VALID_TAG_FRIEND).build();
+    //        DESC_BOB = new EditPersonDescriptorBuilder().withName(VALID_NAME_BOB)
+    //                .withPhone(VALID_PHONE_BOB).withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
+    //                .withTags(VALID_TAG_HUSBAND, VALID_TAG_FRIEND).build();
+    //    }
 
     /**
      * Executes the given {@code command}, confirms that <br>
