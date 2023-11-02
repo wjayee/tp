4a55803be2651aa7320f7ab09d2d50972da34e5b
euchangxian <changxian2001@gmail.com>
2023-11-03 00:08:06 +0800

    Add CommandEnum
    CommandEnum encapsulates a Command's name and help message, retrieved
    using the static getHelp() message.
    
    NOTE:
      For any new commands added, please update CommandEnum so that
      HelpAnimalCommand can retrieve the help message for the user.

---
+++ src/main/java/seedu/address/model/CommandEnum.java
@@ -0,0 +1,40 @@
+package seedu.address.model;
+
+
+import seedu.address.logic.commands.AddAnimalCommand;
+import seedu.address.logic.commands.AddTaskCommand;
+import seedu.address.logic.commands.DeleteAnimalCommand;
+import seedu.address.logic.commands.EditAnimalCommand;
+import seedu.address.logic.commands.ListAnimalCommand;
+import seedu.address.logic.commands.MarkTaskCommand;
+import seedu.address.logic.commands.ResetTaskCommand;
+import seedu.address.logic.commands.SearchAnimalCommand;
+import seedu.address.logic.commands.UnmarkTaskCommand;
+
+public enum CommandEnum {
+    ADD_ANIMAL_COMMAND(AddAnimalCommand.COMMAND_WORD, AddAnimalCommand.getHelp()),
+    ADD_TASK_COMMAND(AddTaskCommand.COMMAND_WORD, AddTaskCommand.getHelp()),
+    DELETE_ANIMAL_COMMAND(DeleteAnimalCommand.COMMAND_WORD, DeleteAnimalCommand.getHelp()),
+    EDIT_ANIMAL_COMMAND(EditAnimalCommand.COMMAND_WORD, EditAnimalCommand.getHelp()),
+    LIST_ANIMAL_COMMAND(ListAnimalCommand.COMMAND_WORD, ListAnimalCommand.getHelp()),
+    MARK_TASK_COMMAND(MarkTaskCommand.COMMAND_WORD, MarkTaskCommand.getHelp()),
+    RESET_TASK_COMMAND(ResetTaskCommand.COMMAND_WORD, ResetTaskCommand.getHelp()),
+    SEARCH_ANIMAL_COMMAND(SearchAnimalCommand.COMMAND_WORD, SearchAnimalCommand.getHelp()),
+    UNMARK_TASK_COMMAND(UnmarkTaskCommand.COMMAND_WORD, UnmarkTaskCommand.getHelp());
+
+    private final String commandWord;
+    private final String helpMessage;
+
+    CommandEnum(String commandWord, String helpMessage) {
+        this.commandWord = commandWord;
+        this.helpMessage = helpMessage;
+    }
+
+    public String getCommandWord() {
+        return commandWord;
+    }
+
+    public String getHelpMessage() {
+        return helpMessage;
+    }
+}
