924da762a5b6c7aeb95a023dedf8e5824415ec7f
junnengsoo <soojunneng01@gmail.com>
2023-11-02 21:08:23 +0800

    Add DeleteTaskCommandParser

--- src/main/java/seedu/address/logic/parser/DeleteTaskCommandParser.java
+++ src/main/java/seedu/address/logic/parser/DeleteTaskCommandParser.java
@@ -3,28 +3,26 @@ package seedu.address.logic.parser;
 import static seedu.address.logic.AnimalMessages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.AddTaskCommand;
 import seedu.address.logic.commands.DeleteTaskCommand;
-import seedu.address.logic.parser.AnimalParserUtil.ParsedTaskInput;
+import seedu.address.logic.parser.AnimalParserUtil.ParsedTwoIndices;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.animal.Task;
 
 /**
- * Parses input arguments and creates a new AddTaskCommand object
+ * Parses input arguments and creates a new DeleteTaskCommand object
  */
 public class DeleteTaskCommandParser implements AnimalParser<DeleteTaskCommand> {
 
     /**
-     * Parses the given {@code String} of arguments in the context of the AddTaskCommand.
-     * and returns an AddTaskCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the DeleteTaskCommand.
+     * and returns an DeleteTaskCommand object for execution.
      *
      * @throws ParseException if the user input does not conform to the expected format.
      */
     public DeleteTaskCommand parse(String args) throws ParseException {
         try {
-            ParsedTaskInput parsedTaskInput = AnimalParserUtil.parseTaskInput(args);
-            Index targetIndex = parsedTaskInput.getTargetIndex();
-            Task taskDescription = parsedTaskInput.getTaskDescription();
+            ParsedTwoIndices parsedTwoIndices = AnimalParserUtil.parseTwoIndices(args);
+            Index targetAnimalIndex = parsedTwoIndices.getFirstIndex();
+            Index targetTaskIndex = parsedTwoIndices.getSecondIndex();
 
             return new DeleteTaskCommand(targetAnimalIndex, targetTaskIndex);
         } catch (ParseException pe) {
