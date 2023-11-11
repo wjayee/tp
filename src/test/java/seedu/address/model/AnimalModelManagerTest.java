9ea49c2f25fe8de115e50725cb6ad276f1e1f2e4
Jason Baek <bkjwjason@gmail.com>
2023-11-11 11:38:27 +0800

    Fix Checkstyle

--- src/test/java/seedu/address/model/AnimalModelManagerTest.java
+++ src/test/java/seedu/address/model/AnimalModelManagerTest.java
@@ -1,35 +1,36 @@
 package seedu.address.model;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import org.mockito.internal.matchers.Null;
+
 import seedu.address.commons.core.GuiSettings;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 public class AnimalModelManagerTest {
-    @TempDir
-    public Path temporaryFolder;
+    private static final AnimalCatalog ANIMAL_CATALOG = new AnimalCatalog();
 
     private static final AnimalUserPrefs ANIMAL_USER_PREFS = new AnimalUserPrefs();
+
     private static final GuiSettings GUI_SETTINGS = new GuiSettings();
 
-    private static final AnimalCatalog ANIMAL_CATALOG = new AnimalCatalog();
+    @TempDir
+    public Path temporaryFolder;
 
     @Test
     public void testSetUserPrefs_withNull() {
         AnimalModelManager animalModelManager = new AnimalModelManager();
-        assertThrows(NullPointerException.class,
-                () -> animalModelManager.setUserPrefs(null));
+        assertThrows(NullPointerException.class, () -> animalModelManager.setUserPrefs(null));
     }
 
     @Test
     public void testSetUserPrefs_withNonNull() {
         AnimalModelManager animalModelManager = new AnimalModelManager();
-        assertDoesNotThrow(
-                () -> animalModelManager.setUserPrefs(ANIMAL_USER_PREFS));
+        assertDoesNotThrow(() -> animalModelManager.setUserPrefs(ANIMAL_USER_PREFS));
     }
 
     @Test
@@ -42,15 +43,13 @@ public class AnimalModelManagerTest {
     @Test
     public void testSetGuiSettings_withNull() {
         AnimalModelManager animalModelManager = new AnimalModelManager();
-        assertThrows(NullPointerException.class,
-                () -> animalModelManager.setGuiSettings(null));
+        assertThrows(NullPointerException.class, () -> animalModelManager.setGuiSettings(null));
     }
 
     @Test
     public void testSetGuiSettings_withNonNull() {
         AnimalModelManager animalModelManager = new AnimalModelManager();
-        assertDoesNotThrow(
-                () -> animalModelManager.setGuiSettings(GUI_SETTINGS));
+        assertDoesNotThrow(() -> animalModelManager.setGuiSettings(GUI_SETTINGS));
     }
 
     @Test
@@ -63,15 +62,13 @@ public class AnimalModelManagerTest {
     @Test
     public void testSetAnimalCatalogFilePath_withNull() {
         AnimalModelManager animalModelManager = new AnimalModelManager();
-        assertThrows(NullPointerException.class,
-                ()-> animalModelManager.setAnimalCatalogFilePath(null));
+        assertThrows(NullPointerException.class, ()-> animalModelManager.setAnimalCatalogFilePath(null));
     }
 
     @Test
     public void testSetAnimalCatalogFilePath_withNonNull() {
         AnimalModelManager animalModelManager = new AnimalModelManager();
-        assertDoesNotThrow(
-                () -> animalModelManager.setAnimalCatalogFilePath(temporaryFolder));
+        assertDoesNotThrow(() -> animalModelManager.setAnimalCatalogFilePath(temporaryFolder));
     }
 
     @Test
